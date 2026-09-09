376b84a34f61ba6fbd9e1d352459e3a7a62bae37
khansakkh <khansaahaikal@gmail.com>
2026-09-09 17:03:11 +0700

    Rename project configuration folder to portofolio

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportofolio.urls'
+ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportofolio.wsgi.application'
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 
 # Database
