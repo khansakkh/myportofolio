582be35c079f82db92d15dd60d7b5fa6d020fa50
khansakkh <khansaahaikal@gmail.com>
2026-09-09 17:35:42 +0700

    Fix view and url wiring to landing_page

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from portofolio.views import show_main
+from portofolio.views import landing_page
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_main, name='show_main'),
+    path('', landing_page, name='landing_page'),
 ]
--- portofolio/views.py
+++ portofolio/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def show_main(request):
-    return render(request, 'index html')
+def landing_page(request):
+    return render(request, "index.html")
