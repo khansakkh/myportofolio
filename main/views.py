e0b58d4f87f96597c6537632c0043b2c831e933d
khansakkh <khansaahaikal@gmail.com>
2026-09-09 23:09:03 +0700

    Menghubungkan View dengan Template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Khansa",
+        "npm": "2506536465",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Undergraduate student of information systems that has interest in "
+            "technology, business, and risk management."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Khansa",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
