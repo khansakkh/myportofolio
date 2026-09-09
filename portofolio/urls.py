1c9f16ade386734912d54475d11cd6f18147ef1f
khansakkh <khansaahaikal@gmail.com>
2026-09-09 17:17:50 +0700

    Add index.html template and render main view

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from portofolio.views import show_main
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', show_main, name='show_main'),
 ]
