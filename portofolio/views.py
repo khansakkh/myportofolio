15e3bd926ddc3bd4e47303aaa2b1127e4fb93d41
khansakkh <khansaahaikal@gmail.com>
2026-09-16 21:35:49 +0700

    define show education

--- portofolio/views.py
+++ portofolio/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import get_object_or_404
 
 def landing_page(request):
     return render(request, "index.html")
+
+def get_education_json(request):
+    search_query = request.GET.get("search", "").strip()
+    education = Education.objects.all()
+
+    if search_query:
+        education = education.filter(institution_name__icontains=search_query)
+
+    education_json = serializers.serialize("json", education)
+    return HttpResponse(education_json, content_type="application/json")
