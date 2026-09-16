bd052274389235f4097977a49f81eafab31e1c6d
khansakkh <khansaahaikal@gmail.com>
2026-09-16 21:39:05 +0700

    add show and del edu to urls, adjustment.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, create_education
+from main.views import (show_main, show_experience, show_education, create_education, get_education_json, delete_education,
+)
 
 app_name = "main"
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path('education/', show_education, name='show_education'),
     path("education/add/", create_education, name="create_education"),
+    path("api/education/", get_education_json, name="get_education_json"),
+    path("education/<int:education_id>/delete/", delete_education, name="delete_education"),
 ]
